76cc9e1e133c198265066534d92601c13d07df8b
millermk <millermk@outlook.com>
2014-09-23 14:33:19 -0600

    Adding the basics of the project
    The underlying framework is there.

---
+++ InTable/InTable/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InTable
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TLFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            TRFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            BRFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+            BLFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+        }
+
+        private void TLButtonClick(object sender, RoutedEventArgs e)
+        {
+            TLFrame.Navigate(new Uri("StartPage.xaml", UriKind.Relative));
+        }
+
+        private void TRButtonClick(object sender, RoutedEventArgs e)
+        {
+            TRFrame.Navigate(new Uri("StartPage.xaml", UriKind.Relative));
+        }
+
+        private void BRButtonClick(object sender, RoutedEventArgs e)
+        {
+            BRFrame.Navigate(new Uri("StartPage.xaml", UriKind.Relative));
+        }
+
+        private void BLButtonClick(object sender, RoutedEventArgs e)
+        {
+            BLFrame.Navigate(new Uri("StartPage.xaml", UriKind.Relative));
+        }
+    }
+}
